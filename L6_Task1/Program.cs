578881c148ba2be6c5da1ab3451b5255c3de1b09
Inessa <inesa-99@mail.ru>
2022-08-14 16:06:51 +0300

    Folder L6_Task1 ready

---
+++ L6_Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+Console.Write("Укажите, сколько чисел вы введете в массив: \t");
+int elementsCount = int.Parse(Console.ReadLine());
+
+int[] myArray = new int[elementsCount];
+
+for (int i=0; i<myArray.Length; i++)
+{
+    Console.Write($"\nВведите элемент массива под индексом {i}:\t");
+    myArray[i] = int.Parse(Console.ReadLine());
+}
+
+int count = 0;
+
+for (int i=0; i<myArray.Length; i++)
+{
+    if (myArray[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"\nКоличество элементов больше 0 = " + count);
